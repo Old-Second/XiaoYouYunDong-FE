74b89b61455e330a40c0d415071e23567e1fe915
Old-Second <oldsecond2@qq.com>
2024-05-06 22:04:06 +0800

    🔧 chore: 添加eslint、Prettier、husky

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,11 @@
-import { Component, PropsWithChildren } from 'react'
-import './app.scss'
+import { Component, PropsWithChildren } from 'react';
+import './app.scss';
 
-class App extends Component<PropsWithChildren>  {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
+class App extends Component<PropsWithChildren> {
   // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
+  render() {
+    return this.props.children;
   }
 }
 
-export default App
+export default App;
