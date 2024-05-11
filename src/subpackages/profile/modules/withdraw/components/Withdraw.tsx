0da0f4c25b9f57888b696e353cf18978d2645376
墨綠BG <s5460703@gmail.com>
2024-05-12 01:37:36 +0800

    ✨ feat：新增提现记录页面

--- src/subpackages/profile/modules/withdraw/components/Withdraw.tsx
+++ src/subpackages/profile/modules/withdraw/components/Withdraw.tsx
@@ -1,50 +1,102 @@
-import { View } from '@tarojs/components';
+import { Button, Text, View } from '@tarojs/components';
+import { useState } from 'react';
+import { AtFloatLayout, AtIcon } from 'taro-ui';
 
 const Withdraw: React.FC = () => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+
+  const date = new Date();
+  const startYear = 2023;
+  const startMonth = 11;
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth() + 1;
+  const currentTime = `${currentYear}年${currentMonth}月`;
+
+  const YearMonthLists: React.ReactNode[] = [];
+  let year = startYear;
+  let month = startMonth;
+
+  while (year <= currentYear) {
+    const monthsInYear: string[] = [];
+    YearMonthLists.push(<Text className="text-[1.8vh] text-gray-500">{year}年</Text>);
+
+    for (month; month >= 1 && month <= 12; month++) {
+      if (year === currentYear && month > currentMonth) {
+        break;
+      }
+      monthsInYear.push(`${month}月`);
+    }
+
+    YearMonthLists.push(
+      <View className="grid grid-cols-4 gap-4">
+        {monthsInYear.map((monthName, index) => (
+          <Button
+            key={index}
+            className="flex w-20 items-center justify-center border-none bg-white px-4 py-1 text-[2.5vh]"
+          >
+            {monthName}
+          </Button>
+        ))}
+      </View>
+    );
+
+    year++;
+    month = 1;
+  }
+
+  const handleAccordionClick = (value: boolean) => {
+    setIsAccordionOpen(value);
+  };
+
   return (
     <View className="relative flex h-screen w-full flex-col bg-gray-100">
-      {/* <View className="relative top-[4vh] flex flex-col items-center gap-2">
-        <View className="flex h-auto w-[90%] flex-col gap-6 rounded-lg bg-white px-5 py-4">
-          <Text className="text-[1.8vh]">总余额</Text>
-          <Eye />
-          <View className="flex flex-col gap-2">
-            <Text className="text-[5vh] font-semibold">$600.00</Text>
-            <View className="relative flex w-full">
-              <Button className="relative left-0 m-0 flex h-10 items-center justify-center border-none bg-[#05D6A1] px-10 py-1 text-[2.5vh] text-white">
-                提现
-              </Button>
-              <Text className="absolute bottom-0 right-0 text-[1.5vh] text-blue-500 underline">
-                提现记录
-              </Text>
+      <View className="flex flex-col items-center gap-4">
+        <View className="relative flex h-auto w-full px-5 pt-4">
+          <Text
+            className="text-[1.8vh]"
+            onClick={() => handleAccordionClick(!isAccordionOpen)}
+          >
+            {currentTime}
+            <AtIcon
+              value={isAccordionOpen ? 'chevron-up' : 'chevron-down'}
+              size={20}
+            ></AtIcon>
+          </Text>
+          <AtFloatLayout
+            isOpened={isAccordionOpen}
+            title="请选择月份"
+            onClose={() => handleAccordionClick(false)}
+          >
+            <View className="flex h-auto w-full flex-col items-center gap-2 px-2 py-3">
+              {YearMonthLists}
             </View>
-            <Text className="text-[1.5vh] text-gray-500">每周二可提现</Text>
-          </View>
+          </AtFloatLayout>
+          <Text className="absolute right-5 text-[1.8vh]">收入 $600.00</Text>
         </View>
         <View className="flex h-auto w-[90%] flex-col gap-6 rounded-lg bg-white px-3 py-4">
-          <Text className="text-[1.8vh]">账单明细</Text>
-          <Eye />
           <View className="flex flex-col items-center gap-2">
             <View className="relative flex h-[10vh] w-full flex-col gap-2 rounded-md border border-gray-300 px-3 py-4">
               <Text className="text-[1.8vh] font-semibold">
-                活动收款（已扣除1%手续费）
+                钱包提现-支付宝15902718000
               </Text>
               <Text className="text-[1.5vh] text-gray-500">2023-01-01 12:00</Text>
               <Text className="absolute bottom-4 right-3 text-[3vh] text-red-500">
-                +300.00
+                300.00
               </Text>
             </View>
             <View className="relative flex h-[10vh] w-full flex-col gap-2 rounded-md border border-gray-300 px-3 py-4">
               <Text className="text-[1.8vh] font-semibold">
-                活动收款（已扣除1%手续费）
+                钱包提现-支付宝15902718000
               </Text>
               <Text className="text-[1.5vh] text-gray-500">2023-01-01 12:00</Text>
               <Text className="absolute bottom-4 right-3 text-[3vh] text-red-500">
-                +300.00
+                300.00
               </Text>
             </View>
           </View>
         </View>
-      </View> */}
+        <Text className="text-[1.5vh] text-gray-500">当月无更多提现记录</Text>
+      </View>
     </View>
   );
 };
