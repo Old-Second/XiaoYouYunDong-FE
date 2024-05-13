288fe4a5c054b36aaf9b7c93945b5c8dc18e520d
Old-Second <oldsecond2@qq.com>
2024-05-14 03:00:22 +0800

    ✨ feat: 新增我的活动页面搜索框

---
+++ src/subpackages/profile/modules/activity/components/ActivitySearchBox.tsx
@@ -0,0 +1,27 @@
+import { Icon, View } from '@tarojs/components';
+import { useState } from 'react';
+import { AtInput } from 'taro-ui';
+
+const ActivitySearchBox: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+  };
+  return (
+    <View className="my-1.5 flex items-center rounded-xl bg-white pl-3">
+      <Icon size="20" type="search" />
+      <AtInput
+        name="searchActivity"
+        type="text"
+        placeholder="搜索活动"
+        confirmType="search"
+        border={false}
+        clear
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+    </View>
+  );
+};
+
+export default ActivitySearchBox;
