7a22d7e20d286ea55ea6b5a7506c4a2c8656b2a2
Old-Second <oldsecond2@qq.com>
2024-05-14 19:26:15 +0800

    ✨ feat: 添加首页Banner

---
+++ src/modules/home/components/HomeBanner.tsx
@@ -0,0 +1,35 @@
+import { CoverImage, Icon, View } from '@tarojs/components';
+import { useState } from 'react';
+import { AtInput } from 'taro-ui';
+
+import 'taro-ui/dist/style/components/icon.scss';
+import 'taro-ui/dist/style/components/input.scss';
+import './custom.scss';
+
+const HomeBanner: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+  };
+  return (
+    <View className="relative h-48">
+      <CoverImage src="https://i0.hdslb.com/bfs/archive/711a2b7ea82cf65d4ca342e9cce07fff846d1292.jpg" />
+      <View className="absolute left-3 top-2 w-1/2 rounded-xl bg-white/[.8] pl-3">
+        <View className="flex h-9 items-center justify-center text-xl">
+          <Icon size="24" type="search" className="h-6" />
+          <AtInput
+            name="searchHome"
+            type="text"
+            confirmType="search"
+            border={false}
+            clear
+            value={searchValue}
+            onChange={handleSearchChange}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HomeBanner;
